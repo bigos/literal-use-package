0f50a1a5b97be99c3afc1f8c2105c38d2e176aad
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2022-09-28 15:15:06 +0100

    corrected the config errors

--- init.el
+++ init.el
@@ -1,5 +1,17 @@
+;; accessing a package repo over https on Windows may fail, so we can
+;; fallback to http there
+(if (eq system-type 'windows-nt)
+    (add-to-list 'package-archives
+                 '("melpa" . "https://melpa.org/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/") t))
+
 (defvar init-dir (file-name-directory load-file-name)
   "Directory of the Emacs config init.el.")
+
+;; set package-user-dir to be relative to init.el path
+(setq package-user-dir (expand-file-name "elpa" init-dir))
+
 
 (defvar org-config (expand-file-name "literal-config.org" init-dir)
   "Path to the literal configuration..")
